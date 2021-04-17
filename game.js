08a9cd436b8a74a90197a24cc4ad80a002831bbc
FominAO <fomagoafterme@yandex.ru>
2021-04-17 22:12:17 +0300

    Add files via upload

---
+++ game.js
@@ -0,0 +1,87 @@
+function roll(a = 0, b = 100) {
+    return Math.round(a + Math.random()*(b-a));
+}
+
+function log(str) {
+    const newLine = document.createElement('div');
+
+    newLine.setAttribute('innerHTML', str);
+    document.body.appendChild(newLine);
+}
+
+const targetsConfig = {
+    head: {
+        hp: 90,
+        accuracy: 0.2,
+        name: 'Голова'
+    },
+    body: {
+        hp: 10,
+        accuracy: 0.7,
+        name: 'Тело'
+    },
+    limbs: {
+        hp: 5,
+        accuracy: 0.1,
+        name: 'Руки/Ноги'
+    },
+}
+
+class Body {
+    targets = [];
+    hp = 1;
+    powerLimit = 1;
+    name = 'NONE';
+    constructor(parts, hp, power, name, conf = targetsConfig) {
+        this.targets = parts.map((part, index) => ({...conf[part], id: index}));
+        this.hp = hp;
+        this.powerLimit = power;
+        this.name = name;
+    }
+
+    shoot(id, power) {
+       const target = this.targets.find(i => i.id === id);
+
+       if (roll(1,10 - 10*target.accuracy) === 10 - 10*target.accuracy) {
+           this.hp -= target.hp*power;
+            console.log(this.hp, target.name, power)
+           
+           return 'Попадание'
+       }
+       return 'Промах'
+    }
+
+    atack() {
+        return roll(0,this.powerLimit) / this.powerLimit;
+    }
+}
+
+const monster = new Body(['head', 'head', 'body', 'limbs', 'limbs', 'limbs', 'limbs', 'limbs', 'limbs', 'limbs'], 25, 30, 'Двухголовый');
+const player = new Body(['head', 'body', 'limbs', 'limbs', 'limbs', 'limbs'], 10, 100, 'Игрок');
+
+function game() {
+    let turn = 'player';
+    let turnsCount = 0;
+
+    document.body.innerHTML = '';
+    while (monster.hp > 0 && player.hp > 0) {
+        const actor = turn === 'player' ? player : monster;
+        const target = turn === 'player' ? monster : player;
+        const targetPartsForChoose = target.targets;
+        const targetPart = targetPartsForChoose[roll(0,targetPartsForChoose.length-1)];
+        const power = actor.atack();
+
+        console.log(`${actor.name} стреляет в ${target.name} (${targetPart.name}). Сила ${power}`);
+        console.log(target.shoot(targetPart.id, power));
+        switchTurn();
+    }
+    const winner = monster.hp > 0 ? monster : player;
+
+    console.log(`Победил ${winner.name} (${winner.hp})! Кол-во ходов: ${turnsCount}`);
+
+    function switchTurn() {
+        turnsCount++;
+        turn = turn === 'player' ? 'monster' : 'player';
+    }
+}
+game();
